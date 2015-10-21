d684ffea7242bc77f78b539914bf9ecdb39e2fcd
Joe Sykes <joe.sykes@coats.com>
2015-10-21 15:48:44 +0100

    TestSuite accessibility

--- src/test/java/com/coats/selenium/tests/Ecomm_SUSST_ME_IT.java
+++ src/test/java/com/coats/selenium/tests/Ecomm_SUSST_ME_IT.java
@@ -19,11 +19,9 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class Ecomm_SUSST_ME_IT extends DriverFactory {
