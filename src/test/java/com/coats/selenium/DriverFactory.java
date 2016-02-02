50fa4174b2bc8ba81be9c0cd890d1b6337368600
azaharia <azaharia@tremend.ro>
2016-02-02 15:34:44 +0200

    updating quickSuite tests

--- src/test/java/com/coats/selenium/DriverFactory.java
+++ src/test/java/com/coats/selenium/DriverFactory.java
@@ -4,6 +4,7 @@ import com.coats.selenium.config.WebDriverThread;
 import com.coats.selenium.listeners.ScreenshotListener;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Listeners;
@@ -21,7 +22,7 @@ public class DriverFactory {
     private static List<WebDriverThread> webDriverThreadPool = Collections.synchronizedList(new ArrayList<WebDriverThread>());
     private static ThreadLocal<WebDriverThread> driverThread;
 
-    @BeforeSuite (alwaysRun=true)
+    @BeforeMethod (alwaysRun=true)
     public static void instantiateDriverObject() {
         driverThread = new ThreadLocal<WebDriverThread>() {
             @Override
