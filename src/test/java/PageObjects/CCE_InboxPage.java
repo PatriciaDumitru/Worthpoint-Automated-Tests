4cdc5b848bf3ce919d2ca72a61b91fe6c2524de9
dion <dion@tremend.ro>
2016-03-22 14:11:59 +0200

    Merge remote-tracking branch 'remotes/origin/master' into Daniel'sBrank
    # Conflicts:
    #	src/test/java/AutomationFramework/DataItems.java
    #	src/test/java/com/coats/selenium/DriverFactory.java
    #	src/test/java/com/coats/selenium/tests/Ecomm_OO_Test.java

--- src/test/java/PageObjects/CCE_InboxPage.java
+++ src/test/java/PageObjects/CCE_InboxPage.java
@@ -36,8 +36,8 @@ public class CCE_InboxPage extends WBA_BasePage {
     By sapStatusField = By.id("s2id_filterSampleOrderLineSapOrderStatusId");
     By requesterField = By.cssSelector("#s2id_filterSampleOrderRequesterId > ul > li > input");
     By viewFrame = By.id("TB_iframeContent");
-    By orderStatusCell = By.xpath(".//*[@id='SampleOrderLineInboxForm']/table/tbody/tr[2]/td[14]");
- 
+    By orderStatusCell = By.xpath("//form[@id='SampleOrderLineInboxForm']/table/tbody/tr[2]/td[14]");
+
     By listOrdersButton = By.cssSelector("#FilterInboxForm > div.actions > ul > li:nth-child(1)");
     By resetButton = By.cssSelector("#FilterInboxForm > div.actions > ul > li:nth-child(2)");
     By viewButton = By.cssSelector("#SampleOrderLineInboxForm > table > tbody > tr:nth-child(2) > td:nth-child(17)");
