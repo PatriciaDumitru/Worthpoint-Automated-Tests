07d6fa83cb46ca25db783960c01acc9dc15a5b23
stefanba81 <sabadalache@gmail.com>
2016-05-16 18:31:11 +0300

    ...

--- src/test/java/com/coats/selenium/DriverFactory.java
+++ src/test/java/com/coats/selenium/DriverFactory.java
@@ -46,7 +46,7 @@ public class DriverFactory {
           System.out.println("----------------------------------------------------");
       }
 
-
+*/
 
       @AfterMethod (alwaysRun=true)
       public static void closeDriverObjects() {
@@ -56,5 +56,5 @@ public class DriverFactory {
         System.out.println("----------------------------------------------------");
 
     }
-*/
+
 }
