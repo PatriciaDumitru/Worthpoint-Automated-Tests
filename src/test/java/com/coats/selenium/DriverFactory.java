681056a0740ede5d0a7f18a6381b25d1337a3017
dion <dion@tremend.ro>
2016-06-02 14:39:03 +0300

    Merge branch 'master' of https://github.com/Coats/WBA-Automated-Scripts .

--- src/test/java/com/coats/selenium/DriverFactory.java
+++ src/test/java/com/coats/selenium/DriverFactory.java
@@ -40,9 +40,7 @@ public class DriverFactory {
           System.out.println("----------------------------------------------------");
       }
 
-
 */
-
 
       @AfterMethod (alwaysRun=true)
       public static void closeDriverObjects() {
