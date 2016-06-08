71005dbc3c6820e1ccdb38a3609b707814e82085
Andrei Zaharia <azaharia@tremend.ro>
2016-06-08 19:39:31 +0300

    close driver comment

--- src/test/java/com/coats/selenium/DriverFactory.java
+++ src/test/java/com/coats/selenium/DriverFactory.java
@@ -41,7 +41,7 @@ public class DriverFactory {
       }
     }
 
-
+*/
       @AfterMethod (alwaysRun=true)
       public static void closeDriverObjects() {
           for (WebDriverThread webDriverThread : webDriverThreadPool) {
@@ -49,5 +49,5 @@ public class DriverFactory {
           }
           System.out.println("----------------------------------------------------");
       }
-*/
+
 }
